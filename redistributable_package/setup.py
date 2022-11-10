6462a2b5b01c1dece06c4974ec2cb1c199b72f0d
Rittwick Bhabak <mcs222054@cse.iitd.ac.in>
2022-11-10 16:42:11 +0530

    redistributable packages made successfully

---
+++ redistributable_package/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup 
+setup(name='tkinter', 
+version="0.1",
+description="Modified Tkinter with a couple of bug fixes and own GraphPlotter",
+long_description="Modified Tkinter with a couple of bug fixes and own GraphPlotter made by Rittwick Bhabak from IIT Delhi",
+author="Rittwick Bhabak",
+packages=['tkinter'],
+install_requires=['Pillow==9.2.0'])
